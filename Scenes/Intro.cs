75338a5346c9c260693d1afeb4f1d93eb7b50bb3
DonmusBorek <109541782+DonmusBorek@users.noreply.github.com>
2022-09-17 17:21:41 +0300

    a

--- Scenes/Intro.cs
+++ Scenes/Intro.cs
@@ -3,30 +3,12 @@ using System;
 
 public class Intro : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    Label Mabel;
-    bool labeliyidir = true;
-    // Called when the node enters the scene tree for the first time.
+
     public override void _Ready()
     {
-        Mabel = GetNode<Label>("Label");
-        Mabel.Text = "ben sinan ugur cocuk severim (ayak) severim";
-
-        Timer timer = this.GetNode<Timer>("Timer");
-        timer.WaitTime = 3;
-        timer.Connect("timeout", this, "on_timeout");
-        timer.Start();
+        var label = GetNode<Label>("Label");
+        label.Text = ("Ben");
     }
 
-    public void on_timeout()
-    {
-        Mabel.Text = "ben sinan ugur yarak severim";
-    }
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        
-    }
+
 }
